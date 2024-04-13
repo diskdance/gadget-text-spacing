79152bba294a917871f3599d5360a6661afb17a4
diskdance <psnbaotg@protonmail.com>
2024-04-13 23:50:54 +0800

    Migrate to ESLint flat config & bump nvm version

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+
+export default tsEslint.config(
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  {
+    ignores: [
+      'dist/',
+    ],
+  },
+  {
+    rules: {
+      semi: 'error',
+      'comma-dangle': ['error', 'always-multiline'],
+      quotes: [
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: true,
+        },
+      ],
+      'max-len': [
+        'error',
+        {
+          code: 100,
+          ignoreComments: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+        },
+      ],
+      'no-constant-condition': [
+        'error',
+        {
+          checkLoops: false,
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'quote-props': ['error', 'as-needed'],
+    },
+  },
+);
